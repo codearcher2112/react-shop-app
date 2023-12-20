3c596205601bd1e0bbd05c48fd3bd3858cebaaa8
Hayk Akunyan <haykak5@gmail.com>
2023-12-20 16:14:03 +0400

    Refactor ( Home Page ) Display product results on Homepage.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,6 @@ import { motion } from 'framer-motion';
 
 const Home = () => {
     const { products } = useContext(ProductContext);
-    console.log(products);
 
     return (
         <motion.main
@@ -19,7 +18,25 @@ const Home = () => {
                 opacity: 0
             }}
         >
-            Home Page
+            <section className="products">
+                <div className="products__container !max-w-7xl container">
+                    <div className="products__inner grid grid-cols-1 gap-5 xs:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
+                        {products.map(product => {
+                            return (
+                                <div className="product-item bg-amber-100" key={product.id}>
+                                    <div className="product-item__image-wrapepr w-full aspect-square">
+                                        <img className="product-item__image block w-full h-full object-contain" src={product.image} alt=""/>
+                                    </div>
+
+                                    <div className="product-item__info">
+                                        {product.title}
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+            </section>
         </motion.main>
     )
 }
