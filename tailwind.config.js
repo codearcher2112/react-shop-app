6cdf700a32adbcb54b50f85e43e2b2fc8b355726
Hayk Akunyan <haykak5@gmail.com>
2023-12-20 16:13:17 +0400

    Feature ( Tailwind Config ) Add new Tailwind CSS configs to tailwind.config.js file, related to container and mobile breakpoints.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,34 @@ export default {
   ],
   theme: {
     extend: {},
+
+    container: {
+      center: true,
+      padding: '1rem',
+    },
+
+    screens: {
+      xxs: '375px',
+      // => @media (min-width: 375px) { ... }
+
+      xs: '544px',
+      // => @media (min-width: 544px) { ... }
+
+      sm: '640px',
+      // => @media (min-width: 640px) { ... }
+
+      md: '768px',
+      // => @media (min-width: 768px) { ... }
+
+      lg: '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      xl: '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
   },
   plugins: [],
 }
