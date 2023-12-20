fc2cfd2f27ea7a0b1c8b0795b69a6842ccf8ed03
Hayk Akunyan <haykak5@gmail.com>
2023-12-20 14:05:35 +0400

    Create ( ProductDetails Page ) Create ProductDetails Page, add route for product item page, minor code cleanup.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+// Pages
 import Home from './pages/Home.jsx';
+import ProductDetails from './pages/ProductDetails.jsx';
+
+// Components
 import Header from './components/Header.jsx';
 import Hero from './components/Hero.jsx';
 import Footer from './components/Footer.jsx';
@@ -12,6 +17,7 @@ function App() {
         <Header />
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/product/:id' element={<ProductDetails />} />
         </Routes>
         <Footer />
       </BrowserRouter>
---
+++ src/pages/ProductDetails.jsx
@@ -0,0 +1,9 @@
+const ProductDetails = () => {
+    return (
+        <div className="product-details">
+            Product Details Page
+        </div>
+    )
+}
+
+export default ProductDetails;
