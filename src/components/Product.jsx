88fd8e99338427aba00cc9fb0c0666671bb0d41a
Hayk Akunyan <haykak5@gmail.com>
2023-12-24 17:31:38 +0400

    Refactor ( Home Page - Product Component ) Make separate Product component for product items.

---
+++ src/components/Product.jsx
@@ -0,0 +1,45 @@
+// import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+
+const Product = ({product}) => {
+    const {id, image, category, title, price} = product;
+
+    return (
+        <div className="product-item bg-amber-100">
+            <div className="product-item__image-wrapepr w-full aspect-square">
+                <img className="product-item__image block w-full h-full object-contain"
+                     src={image}
+                     alt={`${title} - ${category}`}
+                />
+            </div>
+
+            <div className="product-item__info">
+                <div className="product-item__category">
+                    {category}
+                </div>
+
+                <h2 className="product-item__title">
+                    <Link to={`/product/${id}`}>
+                        {title}
+                    </Link>
+                </h2>
+
+                <div className="product-item__price">
+                    $ {price}
+                </div>
+            </div>
+
+            <div className="product-item__buttons-warpper">
+                <button type="button" className="product-item__button uppercase">
+                    Add to cart
+                </button>
+
+                <Link to={`/product/${id}`}>
+                    Learn More
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default Product;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { useContext, } from 'react';
 import { ProductContext, } from '../contexts/ProductContext.jsx';
+import Product from '../components/Product.jsx';
 import { motion } from 'framer-motion';
 
 const Home = () => {
@@ -23,15 +24,7 @@ const Home = () => {
                     <div className="products__inner grid grid-cols-1 gap-5 xs:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
                         {products.map(product => {
                             return (
-                                <div className="product-item bg-amber-100" key={product.id}>
-                                    <div className="product-item__image-wrapepr w-full aspect-square">
-                                        <img className="product-item__image block w-full h-full object-contain" src={product.image} alt=""/>
-                                    </div>
-
-                                    <div className="product-item__info">
-                                        {product.title}
-                                    </div>
-                                </div>
+                                <Product key={product.id} product={product} />
                             )
                         })}
                     </div>
